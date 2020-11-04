7578ece3ba46e96e75ab2aa6bc44ce1f11404845
one-data-cookie <kolacek.m@gmail.com>
2020-11-04 22:14:13 +0100

    switch from my to AE version

--- main.py
+++ main.py
@@ -7,38 +7,39 @@ import os
 
 def brno_part_budget():
 
-    # Get projects data
-    proj_res = rq.get('https://gis.brno.cz/ags1/rest/services/Hosted/ProjektyPARO/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json')
-    proj_data = [i['attributes'] for i in proj_res.json()['features']]
+    # Import data from API
+    response = rq.get('https://gis.brno.cz/ags1/rest/services/Hosted/ProjektyPARO/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json')
+    data = response.json()
+    proj_data = [i['attributes'] for i in data['features']]
+    api_data = pd.DataFrame(proj_data)
+
+    # Scrape web page (property IDs and votes data)
+    pids_data = []
+    votes_data = []
+
+    for year in ['2017', '2018', '2019', '2020']:
+      page_res = rq.get('https://damenavas.brno.cz/vysledky-hlasovani/?y=' + year)
+      soup = bs4.BeautifulSoup(page_res.content, 'html.parser')
+      
+      for projects in soup.find_all('div', attrs={re.compile('col-xs-12 vap-project-name')}):
+        pids = int(re.compile(r'id=(\d{1,})').findall(str(projects.a))[0])
+        pids_data.append(pids)
         
-    # Scrape voting data
-    vote_data = []
-    for year in [2017, 2018, 2019, 2020]:
+      for votes in soup.find_all('span', attrs={'class':'vap-project-balance-number'}):
+        votes = int(votes.text.replace(' ', '')) 
+        votes_data.append(votes)
 
-        # Get BS object
-        vote_res = rq.get('https://damenavas.brno.cz/vysledky-hlasovani/?y=' + str(year))
-        soup = bs4.BeautifulSoup(vote_res.content, 'html.parser')
-        
-        # Extract property IDs
-        pids = soup.find_all('div', attrs={'class':re.compile('col-xs-12 vap-project-name')})
-        vote_pids = [int(re.compile(r'id=(\d{1,})').findall(str(i.a))[0]) for i in pids]
-        
-        # Extract votes
-        votes = soup.find_all('span', attrs={'class':'vap-project-balance-number'})
-        vote_votes = [int(i.text.replace(' ', '')) for i in votes]
+    wp_data = pd.DataFrame(list(zip(pids_data, votes_data)), columns=['properties_id','votes']) 
 
-        # Put together
-        for i, j in zip(vote_pids, vote_votes):
-            vote_data.append({"properties_id": i, "properties_vote": j})
+    # Join data together and clean
+    full_data = api_data.join(wp_data.set_index('properties_id'), on='properties_id')
+    full_data = full_data.fillna('').sort_values('properties_id')
 
-    # Join and clean data
-    df = pd.merge(pd.DataFrame(proj_data), pd.DataFrame(vote_data), how='left', on='properties_id')
-    df = df.sort_values(by=['objectid'], ignore_index=True).fillna('')
-
-    # Open and write into GSheet
+    # Push to GSheet
     gc = gs.service_account(filename=os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
-    ss = gc.open_by_key(os.environ['GOOGLE_SPREADSHEET_ID'])
-    ss.worksheets()[0].update([df.columns.values.tolist()] + df.values.tolist())
+    sh = gc.open_by_key(os.environ['GOOGLE_SPREADSHEET_ID'])
+    ws = sh.get_worksheet(0)
+    ws.update([full_data.columns.values.tolist()] + full_data.values.tolist())
 
     # Return message if successful
     return print('Data successfully updated.')
