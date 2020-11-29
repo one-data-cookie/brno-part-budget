ed345142dab52cfcd854bbbbf4b2b462b2701318
one-data-cookie <kolacek.m@gmail.com>
2020-11-30 00:34:23 +0100

    change cron back to run at 2am

--- cronjob.py
+++ cronjob.py
@@ -2,6 +2,6 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from main import brno_part_budget
 
 sched = BlockingScheduler()
-sched.add_job(brno_part_budget, 'cron', hour=21, minute=15)
+sched.add_job(brno_part_budget, 'cron', hour=2)
 
 sched.start()
